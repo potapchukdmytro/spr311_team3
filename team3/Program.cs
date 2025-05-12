1c71466d8cca376b75bb29fc3077f45d02443021
MISHA-ITS <mybox018@gmail.com>
2025-05-12 21:49:21 +0300

    Add DbContext

--- team3/Program.cs
+++ team3/Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
