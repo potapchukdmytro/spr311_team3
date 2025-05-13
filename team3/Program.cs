a221aa6d226b0f05ea067c24e69e652ce971f9b6
MISHA-ITS <mybox018@gmail.com>
2025-05-13 22:10:07 +0300

    Create DbContext

--- team3/Program.cs
+++ team3/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using team3.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql("name=PostgresLocal");
+});
 
 var app = builder.Build();
 
