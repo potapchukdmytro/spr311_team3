eff961c0cf0dc602f9e645743666c89dcd620128
MISHA-ITS <mybox018@gmail.com>
2025-05-13 22:40:19 +0300

    Add tables

---
+++ team3.DAL/Entities/ProductEntity.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace team3.DAL.Entities;
+
+public class ProductEntity
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public required string Name { get; set; }
+
+    [StringLength(500)]
+    public string? Description { get; set; }
+
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Price { get; set; }
+
+    public int StockQuantity { get; set; }
+
+    [ForeignKey("Category")]
+    public int CategoryId { get; set; }
+
+    public virtual CategoryEntity? Category { get; set; }
+}
