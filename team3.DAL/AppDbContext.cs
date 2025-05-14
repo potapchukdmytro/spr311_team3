545807624d72963db3953521fe1ea4e26c82ff33
MISHA-ITS <mybox018@gmail.com>
2025-05-14 23:36:11 +0300

    Add CategoryRepository

--- team3.DAL/AppDbContext.cs
+++ team3.DAL/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using team3.DAL.Entities;
 
 namespace team3.DAL
 
@@ -7,5 +8,8 @@ namespace team3.DAL
     {
         public AppDbContext(DbContextOptions options)
             : base(options) { }
+
+        public DbSet<CategoryEntity> Categories { get; set; }
+        public DbSet<ProductEntity> Products { get; set; }
     }
 }
---
+++ team3.DAL/Repositories/Category/ICategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using team3.DAL.Entities;
+
+namespace team3.DAL.Repositories.Category
+{
+    public interface ICategoryRepository
+    {
+        Task CreateAsync(CategoryEntity entity);
+        Task UpdateAsync(CategoryEntity entity);
+        Task DeleteAsync(string id);
+        Task<CategoryEntity?> FindByIdAsync(string id);
+        Task<CategoryEntity?> FindByNameAsync(string name);
+        IQueryable<CategoryEntity> GetAll();
+    }
+}
