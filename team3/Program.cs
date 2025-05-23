0e0cf0b2f735dc25fb2913448647a6214754d736
loki00345 <99328513+loki00345@users.noreply.github.com>
2025-05-23 21:48:06 +0300

    Ніби зробив serilog

--- team3/Program.cs
+++ team3/Program.cs
@@ -6,54 +6,84 @@ using FluentValidation;
 using team3.BLL.Validators.Product;
 using team3.BLL.Validators.Category;
 using FluentValidation.AspNetCore;
+using Serilog;
+using Serilog.Events;
+using team3.Middleware;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddFluentValidationAutoValidation();
-builder.Services.AddValidatorsFromAssemblyContaining<CreateProductDtoValidator>();
-builder.Services.AddValidatorsFromAssemblyContaining<CategoryDtoValidator>();
-builder.Services.AddValidatorsFromAssemblyContaining<ProductDtoValidator>();
-
-// Add repositories
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-builder.Services.AddDbContext<AppDbContext>(options =>
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.Console(outputTemplate: "{Timestamp:HH:mm:ss} [{Level}] {Message}{NewLine}{Exception}")
+    .WriteTo.File(
+        path: "Logs/log-.txt",
+        rollingInterval: RollingInterval.Hour,
+        retainedFileCountLimit: 24 * 7,
+        outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level}] {Message}{NewLine}{Exception}")
+    .CreateLogger();
+try
 {
-    options.UseNpgsql("name=PostgresLocal");
-});
+    var builder = WebApplication.CreateBuilder(args);
 
-//Add automapper
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+    builder.Host.UseSerilog();
 
-builder.Services.AddCors(opt =>
-{
-    opt.AddPolicy("react_cors", opt =>
+    builder.Services.AddFluentValidationAutoValidation();
+    builder.Services.AddValidatorsFromAssemblyContaining<CreateProductDtoValidator>();
+    builder.Services.AddValidatorsFromAssemblyContaining<CategoryDtoValidator>();
+    builder.Services.AddValidatorsFromAssemblyContaining<ProductDtoValidator>();
+
+
+    // Add repositories
+    builder.Services.AddScoped<IProductRepository, ProductRepository>();
+    builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+    builder.Services.AddControllers();
+    // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+    builder.Services.AddOpenApi();
+
+    builder.Services.AddDbContext<AppDbContext>(options =>
     {
-        opt
-        .WithOrigins("http://localhost:5173")
-        .AllowAnyHeader()
-        .AllowAnyMethod()
-        .AllowCredentials();
+        options.UseNpgsql("name=PostgresLocal");
     });
-});
 
-var app = builder.Build();
+    //Add automapper
+    builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-// Configure the HTTP request pipeline..
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
+    builder.Services.AddCors(opt =>
+    {
+        opt.AddPolicy("react_cors", opt =>
+        {
+            opt
+            .WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+        });
+    });
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline..
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
+
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Застосунок завершився неочікувано");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
